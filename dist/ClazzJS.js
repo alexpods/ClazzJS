e6bbec674ec676343df7dd518141e0d0efc7e53f
alexpods <aleksey.podskrebyshev@gmail.com>
2013-09-27 01:01:44 +0400

    Fix new instance creation

--- dist/ClazzJS.js
+++ dist/ClazzJS.js
@@ -34,7 +34,9 @@ Base.NAME   = '__BASE_CLAZZ__';
 Base.parent = null;
 
 Base.create = function() {
-    return new this.apply(this, Array.prototype.slice(arguments));
+    // Dirty hack!!!! But I don't know better solution:(
+    var a = arguments;
+    return new this(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10]);
 }
 
 Base.prototype = {
