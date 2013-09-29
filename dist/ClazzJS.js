ed0195a3e76f7d61d218de246f5652496e2d3505
alexpods <aleksey.podskrebyshev@gmail.com>
2013-09-29 11:45:30 +0400

    Clazz interface refactoring

--- dist/ClazzJS.js
+++ dist/ClazzJS.js
@@ -1,34 +1,17 @@
 ;(function(global, Meta, undefined) {
 
 
-var Clazz = function() {
-    var name, parent, meta;
-
-    var first  = arguments[0];
-    var second = arguments[1];
-    var last   = arguments[arguments.length - 1];
-
-    if (typeof first === 'string') {
-        name = first;
+var Clazz = function(name, parent, meta) {
+    if (typeof meta === 'undefined') {
+        meta   = parent;
+        parent = null;
     }
 
-    if ((typeof second === 'string') || (typeof second === 'object' && second.prototype instanceof Base)) {
-        parent = second;
+    if (Object.prototype.toString.call(arguments) === '[object Array]') {
+        return Manager.get(name, /* dependencies */ meta);
     }
 
-    if (last.constructor === {}.constructor || typeof last === 'function') {
-        meta = last;
-    }
-
-    if (meta) {
-        if (!name) {
-            return Factory.create(name, parent, meta, Array.prototype.slice.call(arguments, arguments.length - 1));
-        }
-        Manager.setMeta(name, parent, meta);
-    }
-    else {
-        return Manager.get(name, Array.prototype.slice.call(arguments, 1));
-    }
+    Manager.setMeta(name, parent, meta);
 }
 var NameSpace = function(namespace) {
     if (NameSpace.current() === namespace) {
