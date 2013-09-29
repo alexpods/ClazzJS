130ef0c4c5aff8636d675b4b784a9f5a20c14455
alexpods <aleksey.podskrebyshev@gmail.com>
2013-09-29 12:52:12 +0400

    Just init method for meta support

--- src/Factory.js
+++ src/Factory.js
@@ -69,6 +69,9 @@ var Factory = {
         if (typeof meta === 'function') {
             meta = meta.apply(clazz)
         }
+        if (typeof meta === 'function') {
+            meta = { methods: { init: meta }}
+        }
 
         if (meta) {
             Clazz.Meta.Clazz.process(clazz, meta);
