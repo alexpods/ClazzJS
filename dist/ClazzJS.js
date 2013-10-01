16b1040ab86f977f6f7da835733df396a9d330b3
alexpods <aleksey.podskrebyshev@gmail.com>
2013-10-01 19:18:39 +0400

    Auto methods generation

--- dist/ClazzJS.js
+++ dist/ClazzJS.js
@@ -460,11 +460,22 @@ var PropertiesDefaultsProcessor = {
     }
 }
 var PropertiesInitProcessor = function(object, properties) {
+    var property, meta;
 
-    for (var property in properties) {
+    for (property in properties) {
         object['_' + property] = undefined;
-    }
 
+        if (Object.prototype.toString.call(properties[property]) === '[object Array]') {
+            properties[property] = { type: properties[property] }
+        }
+        else if (typeof meta !== 'object' || meta === null) {
+            properties[property] = { default: properties[property] }
+        }
+
+        if (!('methods' in properties[property])) {
+            properties[property].methods = ['get', 'set', 'has', 'is']
+        }
+    }
 }
 var PropertiesInterfaceProcessor = new Meta.Processors.Interface({
 
--- src/MetaProcessors/PropertiesInitProcessor.js
+++ src/MetaProcessors/PropertiesInitProcessor.js
@@ -1,7 +1,18 @@
 var PropertiesInitProcessor = function(object, properties) {
+    var property, meta;
 
-    for (var property in properties) {
+    for (property in properties) {
         object['_' + property] = undefined;
-    }
 
+        if (Object.prototype.toString.call(properties[property]) === '[object Array]') {
+            properties[property] = { type: properties[property] }
+        }
+        else if (typeof meta !== 'object' || meta === null) {
+            properties[property] = { default: properties[property] }
+        }
+
+        if (!('methods' in properties[property])) {
+            properties[property].methods = ['get', 'set', 'has', 'is']
+        }
+    }
 }
